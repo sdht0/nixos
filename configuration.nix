0c3b8fe58225284208f3d93dee6be96460b0c7cb
Siddhartha <dev@sdht.in>
2023-08-16 16:25:16 -0400

    Initial NixOS

---
+++ configuration.nix
@@ -0,0 +1,61 @@
+{ config, pkgs, ... }:
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  networking.hostName = "niximaeus";
+  time.timeZone = "America/Toronto";
+  i18n.defaultLocale = "en_CA.UTF-8";
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/efi";
+
+  users.users.sdh = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
+
+  services.xserver.enable = true;
+  services.xserver.libinput.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+
+  services.xserver.desktopManager.plasma5.enable = true;
+  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+    elisa
+    oxygen
+  ];
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+    #jack.enable = true;
+  };
+
+  networking.networkmanager.enable = true;
+  networking.firewall.enable = false;
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+  ];
+
+  services.openssh.enable = true;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+}
