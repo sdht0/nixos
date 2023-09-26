86e5e7abcf2f09f81d47f7cab3c76dc0b492983d
Siddhartha <dev@sdht.in>
2023-09-26 11:55:42 -0400

    Update

--- configuration.nix
+++ configuration.nix
@@ -13,13 +13,26 @@
   boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/efi";
+  boot.kernelParams = [ "quiet" ];
+  hardware.cpu.intel.updateMicrocode = true;
+  services.fwupd.enable = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   users.users.sdh = {
     isNormalUser = true;
+    shell = pkgs.zsh;
     extraGroups = [ "wheel" ];
   };
 
   services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "modesetting" ];
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+    ];
+  };
   services.xserver.libinput.enable = true;
   services.xserver.displayManager.sddm.enable = true;
 
@@ -41,21 +54,36 @@
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
-    #jack.enable = true;
+    jack.enable = true;
   };
 
   networking.networkmanager.enable = true;
   networking.firewall.enable = false;
+  services.resolved = {
+    enable = true;
+    dnssec = "false";
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+  };
 
   nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   environment.systemPackages = with pkgs; [
-    vim
-    wget
-    zsh
-    git
-    yakuakeee
-    firefox
-    vscode
+    linux-firmware sof-firmware
+    vlc
+    yakuake konsole kate
+    vim wget curl rsync git tmux htop ripgrep fzf eza peco sshfs
+    firefox chromium
+    vscode obsidian calibre tailscale
+    ffmpeg
+  ];
+  programs.zsh.enable = true;
+  environment.shells = with pkgs; [ zsh ];
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    meslo-lgs-nf
   ];
   services.openssh.enable = true;
 
