26844f8b94d814798aa0a718fcf3f87561ef759f
Siddhartha <dev@sdht.in>
2023-09-29 15:36:31 -0400

    Fix

--- configs/system/systemd-boot/custom-systemd-boot.nix
+++ configs/system/systemd-boot/custom-systemd-boot.nix
@@ -1,5 +1,5 @@
 { config, lib, pkgs, ... }:
 {
   disabledModules = [ "system/boot/loader/systemd-boot/systemd-boot.nix" ];
-  imports = [ ./systemd-boot/systemd-boot.nix ];
+  imports = [ ./systemd-boot.nix ];
 }
