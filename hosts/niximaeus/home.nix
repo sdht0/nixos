e8b8f9e033813dd3ebf25ebb996864296e054765
Siddhartha <dev@sdht.in>
2023-09-29 16:48:15 -0400

    Links

--- hosts/niximaeus/home.nix
+++ hosts/niximaeus/home.nix
@@ -28,6 +28,18 @@
   home.file.".ssh/id_ed25519.pub".source =
     config.lib.file.mkOutOfStoreSymlink  "${config.home.homeDirectory}/.dotfiles.safe/ssh/id_ed25519.pub";
 
+  home.file.".config/plasma-workspace/env/ssh-agent.sh".source =
+    config.lib.file.mkOutOfStoreSymlink  "${config.home.homeDirectory}/.dotfiles/ssh_agent/ssh-agent.sh";
+
+  home.file.".config/autostart/ssh-add.sh.desktop".source =
+    config.lib.file.mkOutOfStoreSymlink  "${config.home.homeDirectory}/.dotfiles.safe/apps/ssh-add.sh.desktop";
+
+  home.file.".local/share/applications/ff2.desktop".source =
+    config.lib.file.mkOutOfStoreSymlink  "${config.home.homeDirectory}/.dotfiles.safe/apps/ff2.desktop";
+
+  home.file.".local/share/applications/ff2nix.desktop".source =
+    config.lib.file.mkOutOfStoreSymlink  "${config.home.homeDirectory}/.dotfiles.safe/apps/ff2nix.desktop";
+
   programs.home-manager.enable = true;
   home.stateVersion = "23.05";
 }
