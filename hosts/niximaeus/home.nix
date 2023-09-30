fbaee0d006dc80b20a002c5e77fa24446ef37ecd
Siddhartha <dev@sdht.in>
2023-09-30 16:45:24 -0400

    Autostart apps

--- hosts/niximaeus/home.nix
+++ hosts/niximaeus/home.nix
@@ -9,14 +9,16 @@ lib.recursiveUpdate {
   programs.home-manager.enable = true;
   home.stateVersion = "23.05";
 }
-(lib'.linkFiles [
+(lib'.linkFiles ([
+  { link = ".config/plasma-workspace/env/ssh-agent.sh"; dest = ".dotfiles/scripts/ssh-agent/ssh-agent.sh"; }
+  { link = ".config/autostart/ssh-add.sh.desktop";      dest = ".dotfiles/scripts/ssh-agent/ssh-add.sh.desktop"; }
+
   { link = ".ssh/config";         dest = ".dotfiles.safe/ssh/config"; }
   { link = ".ssh/id_ed25519";     dest = ".dotfiles.safe/ssh/id_ed25519"; }
   { link = ".ssh/id_ed25519.pub"; dest = ".dotfiles.safe/ssh/id_ed25519.pub"; }
 
-  { link = ".config/plasma-workspace/env/ssh-agent.sh"; dest = ".dotfiles/ssh_agent/ssh-agent.sh"; }
-  { link = ".config/autostart/ssh-add.sh.desktop";      dest = ".dotfiles.safe/apps/ssh-add.sh.desktop"; }
-
   { link = ".local/share/applications/ff2.desktop";     dest = ".dotfiles.safe/apps/ff2.desktop"; }
   { link = ".local/share/applications/ff2nix.desktop";  dest = ".dotfiles.safe/apps/ff2nix.desktop"; }
-])
+] ++ (map lib'.autostartApplication [
+  "org.kde.yakuake.desktop" "solaar.desktop" "aw-qt.desktop" "mullvad-vpn.desktop"
+])))
--- lib/default.nix
+++ lib/default.nix
@@ -1,10 +1,22 @@
 { lib, config }:
 {
+  # Creates links from a list [{ link = ...; dest = ...; } ...]
   linkFiles = list:
     builtins.foldl'
-      (set: { link, dest }: lib.recursiveUpdate set {
-        home.file.${link}.source =
-          config.lib.file.mkOutOfStoreSymlink  "${config.home.homeDirectory}/${dest}";
-      })
+      (set: { link, dest }:
+        let
+          # Prefix home directory if `dest` is not an absolute path
+          prefix = if (lib.hasPrefix "/" dest) then "" else "${config.home.homeDirectory}/";
+        in
+          # Deeply merge attribute names
+          lib.recursiveUpdate set {
+            home.file.${link}.source =
+              config.lib.file.mkOutOfStoreSymlink  "${prefix}${dest}";
+          })
       {} list;
+
+  autostartApplication = app: {
+    link = ".config/autostart/${app}";
+    dest = "/run/current-system/sw/share/applications/${app}";
+  };
 }
