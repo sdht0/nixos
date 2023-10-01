1615b851e55517747d3dfc78983a4b03727660a7
Siddhartha <dev@sdht.in>
2023-10-01 13:22:39 -0400

    Fix

--- configs/system/zfs.nix
+++ configs/system/zfs.nix
@@ -6,13 +6,14 @@
   networking.hostId = "3eb4f87f";
 
   services.sanoid.enable = true;
-  services.sanoid.interval = "daily";
+  services.sanoid.interval = "hourly";
   services.sanoid.datasets.medialand = {
-    recursive = "zfs";
+    recursive = true;
     processChildrenOnly = true;
     autoprune = true;
     autosnap = true;
     daily = 7;
     monthly = 5;
+    hourly = 2;
   };
 }
