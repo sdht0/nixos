c9b8a73dd4fab81ab88ac1e1fa6ab133d92eaaa5
Siddhartha <dev@sdht.in>
2023-10-10 16:34:38 -0400

    Solaar override not working, disable for now

--- configs/system/logitech.nix
+++ configs/system/logitech.nix
@@ -3,12 +3,13 @@
 {
   hardware.logitech.wireless.enable = true;
 
-  nixpkgs.overlays = [(final: prev: {
-    solaar-mod = prev.solaar.overrideAttrs (old: {
-      postFixup = ''
-        sed -i 's/Exec=solaar/Exec=solaar --window hide/' $out/share/applications/solaar.desktop
-      '';
-    });
-  })];
-  environment.systemPackages = with pkgs; [ solaar-mod ];
+  # nixpkgs.overlays = [(final: prev: {
+  #   solaar-mod = prev.solaar.overrideAttrs (old: {
+  #     name = "solaar-mod-${old.version}";
+  #     postFixup = ''
+  #       sed -i 's/Exec=solaar/Exec=solaar --window hide/' $out/share/applications/solaar.desktop
+  #     '';
+  #   });
+  # })];
+  environment.systemPackages = with pkgs; [ solaar ];
 }
