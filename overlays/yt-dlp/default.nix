1eba86a62d9302b976913ef2df0bccd6519c8a66
Siddhartha <dev@sdht.in>
2023-11-15 10:26:37 -0500

    Update

--- overlays/yt-dlp/default.nix
+++ overlays/yt-dlp/default.nix
@@ -2,12 +2,12 @@
 
 (final: prev: {
   yt-dlp-git = prev.yt-dlp.overrideAttrs (old: {
-    name = "yt-dlp-git-cc8d844";
+    name = "yt-dlp-git-21dc06";
     src = pkgs.fetchFromGitHub {
       owner  = "yt-dlp";
       repo   = "yt-dlp";
-      rev    = "cc8d8441524ec3442d7c0d3f8f33f15b66aa06f3";
-      sha256 = "sha256-/R5fLzmpywl4ureGS7j/MRd7Y0jGZpJ9dEuXM333kzs=";
+      rev    = "21dc069bea2d4d99345dd969e098f4535c751d45";
+      sha256 = "sha256-ZvMvDi+Wolh61yyvZEvhJYtYC0Q5neeAchFMpuZqnws=";
     };
     patches = [ ./enhance.patch ];
   });
