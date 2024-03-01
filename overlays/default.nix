7b5cc8157e84006a2d1037f72480742e4558bc21
Siddhartha <dev@sdht.in>
2024-03-01 10:32:12 -0500

    pkgs: remove unneeded overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -16,35 +16,12 @@
     });
   })
   (final: prev: {
-    rust-rover = prev.jetbrains.rust-rover.overrideAttrs (old: {
-      name = "overlay-${old.pname}-${old.version}";
-      src = pkgs.fetchurl {
-        url = "https://download.jetbrains.com/rustrover/RustRover-233.14015.147.tar.gz";
-        sha256 = "356c0d491045097f1b7fc82368d1b34c8025edc029a004c7dad47746834df083";
-      };
-      buildInputs = old.buildInputs ++ [prev.xorg.libX11 prev.libGL prev.fontconfig];
-      postPatch = builtins.replaceStrings ["patch"] ["true #patch"] old.postPatch;
-    });
-  })
-  (final: prev: {
     solaar = prev.solaar.overrideAttrs (old: {
       name = "overlay-${old.pname}-${old.version}";
       postFixup = ''
         ${old.postFixup}
         sed -i 's/Exec=solaar/Exec=solaar --window hide/' $out/share/applications/solaar.desktop
       '';
-    });
-  })
-  (final: prev: {
-    obsidian = (prev.obsidian.override {
-        electron = pkgs.electron_28;
-      }).overrideAttrs (old: rec {
-        version = "1.5.8";
-        name = "overlay-${old.pname}-${version}";
-        src = pkgs.fetchurl {
-          url = "https://github.com/obsidianmd/obsidian-releases/releases/download/v${version}/obsidian-${version}.tar.gz";
-          hash = "sha256-oc2iA2E3ac/uUNv6unzfac5meHqQzmzDVl/M9jNpS/M=";
-        };
     });
   })
 ]
