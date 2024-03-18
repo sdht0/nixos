03ca9615727481572894932fd3ce1f18d229ee19
Siddhartha <dev@sdht.in>
2024-03-18 02:24:27 -0400

    pkgs(yt-dlp): update

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,8 +6,8 @@
       src = pkgs.fetchFromGitHub {
         owner  = "yt-dlp";
         repo   = "yt-dlp";
-        rev    = "d63eae7e7ffb1f3e733e552b9e5e82355bfba214";
-        sha256 = "sha256-HncTDF+YbRBs8KyL76+fuH6iQuhV4PTNSGH7/fI/RN0=";
+        rev    = "f849d77ab54788446b995d256e1ee0894c4fb927";
+        hash = "sha256-tfRQUbZYzknDqprYfTEAJrMAdhIeFysEuQnjZyX/QAU=";
       };
       patches = [ ./files/yt-dlp-enhance.patch ];
     });
