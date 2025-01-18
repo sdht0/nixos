ec7b674f13890b128588d62dd905874603b7d4e7
Siddhartha Sahu <dev@sdht.in>
2025-01-18 16:35:27 -0500

    nix: enable rebuilt ng

--- modules/system-nix-linux.nix
+++ modules/system-nix-linux.nix
@@ -8,10 +8,9 @@
 {
   systemd.services.nix-daemon.environment.TMPDIR = "/var/tmp/nix-daemon";
 
-  system.switch = {
-    enable = false;
-    enableNg = true;
-  };
+  system.switch.enable = false;
+  system.switch.enableNg = true;
+  system.rebuild.enableNg = true;
 
   nix.gc.automatic = true;
   nix.gc.dates = "daily";
