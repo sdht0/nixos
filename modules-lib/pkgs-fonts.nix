625f3167c208cdac66423969b0292029a30ca849
Siddhartha Sahu <dev@sdht.in>
2025-02-10 11:22:07 -0500

    feat: switch fonts

--- modules-lib/pkgs-fonts.nix
+++ modules-lib/pkgs-fonts.nix
@@ -1,8 +1,7 @@
 { config, pkgs, ... }:
 {
   fonts.packages = with pkgs; [
-    jetbrains-mono
-    meslo-lgs-nf
+    nerd-fonts.jetbrains-mono
     noto-fonts
     noto-fonts-cjk-sans
     noto-fonts-color-emoji
