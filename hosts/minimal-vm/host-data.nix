f9d68e0bb06073efc57d5c885434545fde3e493c
Siddhartha Sahu <dev@sdht.in>
2025-04-05 14:18:31 -0400

    host: init stripped down bootable minimal vm

---
+++ hosts/minimal-vm/host-data.nix
@@ -0,0 +1,6 @@
+{
+  system = "x86_64-linux";
+  timezone = "America/Toronto";
+  locale = "en_CA.UTF-8";
+  noHm = true;
+}
---
+++ hosts/minimal-vm/modules/configuration.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  lib,
+  pkgs,
+  hostData,
+  ...
+}:
+{
+  documentation.doc.enable = false;
+  nix.enable = false;
+  system.switch.enableNg = false;
+  system.rebuild.enableNg = false;
+  boot.initrd.systemd.enable = false;
+  programs.nano.enable = false;
+  security.sudo.enable = false;
+  programs.command-not-found.enable = false;
+  fonts.fontconfig.enable = false;
+  services.xserver.enable = false;
+
+  system.switch.enable = true;
+
+  nixpkgs.hostPlatform = hostData.system;
+  networking.hostName = hostData.hostname;
+  time.timeZone = hostData.timezone;
+  i18n.defaultLocale = hostData.locale;
+
+  system.stateVersion = "24.05";
+}
