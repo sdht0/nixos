5ec16332148586d782cce51e13a7f36663ded240
Siddhartha Sahu <dev@sdht.in>
2025-04-05 14:17:12 -0400

    sshd: turn on x11 forwarding

--- modules-lib/networking-sshd.nix
+++ modules-lib/networking-sshd.nix
@@ -7,8 +7,11 @@
 
 {
   services.sshd.enable = true;
-  services.openssh.settings.PasswordAuthentication = false;
-  services.openssh.settings.PermitRootLogin = "no";
+  services.openssh.settings = {
+    PasswordAuthentication = false;
+    PermitRootLogin = "no";
+    X11Forwarding = true;
+  };
   users.users.${hostData.users.mainuser.username}.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBKEvNaKu4pI+juLT91YpbON3ell6lQ8A+2rItGP2mUE artimaeus"
   ];
