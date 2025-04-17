89026ce039d65235a2344ac97e8cbb499d2dacd9
Siddhartha Sahu <dev@sdht.in>
2025-04-16 21:43:03 -0400

    Revert "trim pkgs"
    This reverts commit d95b9238bd9c4a30a99afab6e7c04a91a89ee57c.

--- modules-lib/system-nix-ld.nix
+++ modules-lib/system-nix-ld.nix
@@ -1,6 +1,7 @@
 {
   config,
   pkgs,
+  hostData,
   ...
 }:
 
@@ -8,32 +9,87 @@
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
     alsa-lib
+    at-spi2-atk
+    at-spi2-core
+    atk
     bzip2
     cairo
     cups
+    curlWithGnuTls
+    dbus
+    dbus-glib
+    desktop-file-utils
+    e2fsprogs
     expat
+    flac
+    fontconfig
+    freeglut
+    freetype
+    fribidi
+    fuse
+    fuse3
+    gdk-pixbuf
+    glew110
     glib
+    gmp
     gst_all_1.gst-plugins-base
     gst_all_1.gst-plugins-ugly
     gst_all_1.gstreamer
+    gtk2
     harfbuzz
     icu
+    keyutils.lib
     libappindicator-gtk2
+    libcaca
+    libcanberra
+    libcap
+    libclang.lib
+    libdbusmenu
     libdrm
     libgcrypt
-    libgbm
     libGL
-    libva
+    libGLU
+    libgpg-error
+    libidn
+    libjack2
+    libjpeg
+    libmikmod
+    libogg
+    libpng12
+    libpulseaudio
+    librsvg
+    libsamplerate
+    libthai
+    libtheora
+    libtiff
+    libudev0-shim
+    libusb1
+    libuuid
+    libvdpau
+    libvorbis
+    libvpx
+    libxcrypt-legacy
+    libxkbcommon
     libxml2
     mesa
+    nspr
     nss
     openssl
     p11-kit
+    pango
+    pixman
     python3
+    SDL2
+    SDL2_image
+    SDL2_mixer
+    SDL2_ttf
+    speex
     stdenv.cc.cc
     stdenv.cc.cc.lib
+    tbb
     udev
     vulkan-loader
+    wayland
     xorg.libICE
     xorg.libpciaccess
     xorg.libSM
