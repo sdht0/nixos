0fc7ee4b8a9da2179588e20a28d50b1defc7ff24
Siddhartha Sahu <dev@sdht.in>
2025-10-08 11:09:05 -0400

    pkg: fix IP

--- hosts/medialando/system/extra-syncthing-mount.nix
+++ hosts/medialando/system/extra-syncthing-mount.nix
@@ -9,5 +9,5 @@
     bindsTo = [ "opt-mnt-syncs.mount" ];
     after = [ "opt-mnt-syncs.mount" ];
   };
-  services.syncthing.guiAddress = "100.80.143.36:8384";
+  services.syncthing.guiAddress = "100.113.188.30:8384";
 }
