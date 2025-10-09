6edf62a84648a6e547fd2fab64ea7d3354fbb5c4
Siddhartha Sahu <dev@sdht.in>
2025-10-09 11:08:10 -0400

    pkg: update config

--- hosts/medialando/system/extra-syncthing-mount.nix
+++ hosts/medialando/system/extra-syncthing-mount.nix
@@ -6,8 +6,8 @@
 }:
 {
   systemd.services.syncthing = {
-    bindsTo = [ "opt-mnt-syncs.mount" ];
-    after = [ "opt-mnt-syncs.mount" ];
+    bindsTo = [ "opt-mnt-backups.mount" ];
+    after = [ "opt-mnt-backups.mount" ];
   };
   services.syncthing.guiAddress = "100.113.188.30:8384";
 }
