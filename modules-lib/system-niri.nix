35149e422d9d7152a091c3f7f520802e103defc4
Siddhartha Sahu <dev@sdht.in>
2025-10-11 08:39:53 -0400

    modules: init niri

---
+++ modules-lib/system-niri.nix
@@ -0,0 +1,32 @@
+{
+  pkgs,
+  ...
+}:
+{
+  programs.niri.enable = true;
+  programs.xwayland.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    alacritty # terminal
+    xwayland-satellite
+    brightnessctl
+    cliphist
+    dunst
+    fuzzel # application launcher
+    niriswitcher
+    pamixer
+    pwvucontrol
+    swaybg # wallpaper
+    swayidle # idle management
+    swaylock-effects
+    swww
+    wlogout
+    swaylock # screen locker
+    udiskie
+    waybar # top bar
+  ];
+
+  services.libinput.enable = true;
+}
