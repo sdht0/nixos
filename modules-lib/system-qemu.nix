9852f33ee495c8669623cda2de60a42d519ca2a4
Siddhartha Sahu <dev@sdht.in>
2025-10-13 15:41:37 -0400

    systemd: fix target name

--- modules-lib/system-qemu.nix
+++ modules-lib/system-qemu.nix
@@ -6,7 +6,7 @@
 
   systemd.user.services.spice-vdagent = {
     description = "spice-vdagent user daemon";
-    after = [ "spice-vdagentd.service" "graphical-session-pre.target" ];
+    after = [ "spice-vdagentd.service" "graphical-session.target" ];
     wants = [ "graphical-session.target" ];
     wantedBy = [ "graphical-session.target" ];
     serviceConfig = {
