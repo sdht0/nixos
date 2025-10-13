54c748710ab4162f9130c66c658cfcf3797d29f2
Siddhartha Sahu <dev@sdht.in>
2025-10-13 15:19:46 -0400

    pkg: fix spice agent

--- modules-lib/system-qemu.nix
+++ modules-lib/system-qemu.nix
@@ -1,6 +1,19 @@
-{ modulesPath, ... }:
+{ modulesPath, pkgs, ... }:
 {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
   services.qemuGuest.enable = true;
   services.spice-vdagentd.enable = true;
+
+  systemd.user.services.spice-vdagent = {
+    description = "spice-vdagent user daemon";
+    after = [ "spice-vdagentd.service" "graphical-session-pre.target" ];
+    wants = [ "graphical-session.target" ];
+    wantedBy = [ "graphical-session.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.spice-vdagent}/bin/spice-vdagent -x";
+    };
+    unitConfig = {
+      ConditionPathExists = "/run/spice-vdagentd/spice-vdagent-sock";
+    };
+  };
 }
