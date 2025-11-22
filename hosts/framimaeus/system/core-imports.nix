70c2e4a9fc088ba6665f070c5b1848a169ff9734
Siddhartha Sahu <dev@sdht.in>
2025-11-22 13:49:14 -0500

    host: mod to server

--- hosts/framimaeus/system/core-imports.nix
+++ hosts/framimaeus/system/core-imports.nix
@@ -10,30 +10,24 @@
     ../../../modules-lib/hardware-firmware.nix
     ../../../modules-lib/hardware-ssd.nix
 
-    ../../../modules-lib/system-zfs.nix
-    ../../../modules-lib/system-plasma6.nix
-    ../../../modules-lib/system-audio.nix
-    ../../../modules-lib/system-bluetooth.nix
     ../../../modules-lib/system-basic-linux.nix
     ../../../modules-lib/system-initrd-systemd.nix
-    ../../../modules-lib/system-oom.nix
     ../../../modules-lib/system-systemd-boot.nix
     ../../../modules-lib/system-users.nix
-    ../../../modules-lib/system-users-autologin.nix
+    ../../../modules-lib/system-zfs.nix
+    ../../../modules-lib/system-oom.nix
     ../../../modules-lib/system-watchdog-disable.nix
 
     ../../../modules-lib/networking-networkmanager.nix
+    ../../../modules-lib/networking-mullvad.nix
     ../../../modules-lib/networking-sshd.nix
     ../../../modules-lib/networking-tailscale.nix
 
     ../../../modules-lib/pkgs-common-linux.nix
-    ../../../modules-lib/pkgs-nix-ld.nix
-    ../../../modules-lib/pkgs-fonts.nix
-    ../../../modules-lib/pkgs-sshagent.nix
-    ../../../modules-lib/pkgs-debuginfod.nix
-    ../../../modules-lib/pkgs-podman.nix
-    ../../../modules-lib/pkgs-dev-nix.nix
-    ../../../modules-lib/pkgs-selfwatcher.nix
+    ../../../modules-lib/pkgs-syncthing.nix
+    ../../../modules-lib/pkgs-docker.nix
     ../../../modules-lib/pkgs-ollama.nix
+    ../../../modules-lib/pkgs-graphics.nix
+    ../../../modules-lib/pkgs-beancount.nix
   ];
 }
