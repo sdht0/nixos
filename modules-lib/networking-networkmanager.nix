149d046800d940bcf26ad5f8ee12cff18bad1c94
Siddhartha Sahu <dev@sdht.in>
2026-02-20 22:51:18 -0500

    config: try

--- modules-lib/networking-networkmanager.nix
+++ modules-lib/networking-networkmanager.nix
@@ -12,7 +12,7 @@
       "1.1.1.1"
       "8.8.8.8"
     ];
-    plugins = lib.mkForce [ ]; # Disable default plugins
+    plugins = lib.mkForce [ pkgs.networkmanager-openvpn ]; # Disable default plugins
   };
   networking.firewall.enable = false;
   services.resolved = {
